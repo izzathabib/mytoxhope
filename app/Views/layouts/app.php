7ec03272f645b753bc19b221aff40b5969d48d1e
izzathabib <muhdizat.h@gmail.com>
2024-06-07 16:46:08 +0800

    Insert default shield register and login view to customize view

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>MyToxHope</title>
+    <title><?= $this->renderSection('title') ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <base href="<?= base_url('public/assets') ?>/" >
