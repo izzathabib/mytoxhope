055358d64100ef8870ce95780a41f01a14eda37e
izzathabib <muhdizat.h@gmail.com>
2024-08-08 21:20:03 +0800

    Change font to use open sans

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.bootstrap5.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <style>
       /* home page css */
       .card-container {
@@ -66,7 +66,7 @@
       }
 
       html * {
-        font-family: "Roboto", Helvetica, sans-serif;
+        font-family: "Open Sans", Helvetica, sans-serif;
       }
 
       body {
